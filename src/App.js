3a7dd2c3e175d1ab89b5abfc7df93d7d1054013b
ETIM-PAUL <etimpaul22@gmail.com>
2022-10-07 03:33:17 +0100

    layout project structure and added deps

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { FiSettings } from 'react-icons/fi';
+import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 
 function App() {
+  const activeMenu = true;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+        <div className="flex relative dark:bg-main-dark-bg">
+          <div
+            className="fixed right-4 bottom-4"
+            style={{
+              zIndex: '1000',
+            }}
+          >
+            <TooltipComponent content="Settings" position="Top">
+              <button type="button" className="text-3xl p-3 hover:drop-shadow-xl hover:bg-light-gray text-white rounded-[50%]" style={{ backgroundColor: 'blue' }}>
+                <FiSettings />
+              </button>
+            </TooltipComponent>
+          </div>
+          {activeMenu ? (
+            <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white">Sidebar</div>
+          ) : (
+            <div className="w-0 dark:bg-secondary-dark-bg" />
+          )}
+          <div className={`dark:bg-main-bg bg-main-bg min-h-screen w-full ${activeMenu ? 'md:ml-72 ' : 'flex-2'}`}>
+            <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar full">Navbar</div>
+          </div>
+
+          <div>
+            <Routes>
+              <Route path="/" element="ECommerce" />
+              <Route path="/ecommerce" element="ECommerce" />
+            </Routes>
+          </div>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
