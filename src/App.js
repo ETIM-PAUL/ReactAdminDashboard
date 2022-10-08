60cd5672917d7b4396eed4cff3b7796578721ba6
ETIM-PAUL <etimpaul22@gmail.com>
2022-10-08 01:27:05 +0100

    built sidebar

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { FiSettings } from 'react-icons/fi';
 import { TooltipComponent } from '@syncfusion/ej2-react-popups';
+import { SideBar } from './components/SideBar';
+import { NavBar } from './components/NavBar';
 
 function App() {
   const activeMenu = true;
@@ -22,12 +24,12 @@ function App() {
             </TooltipComponent>
           </div>
           {activeMenu ? (
-            <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white">Sidebar</div>
+            <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white"><SideBar /></div>
           ) : (
             <div className="w-0 dark:bg-secondary-dark-bg" />
           )}
           <div className={`dark:bg-main-bg bg-main-bg min-h-screen w-full ${activeMenu ? 'md:ml-72 ' : 'flex-2'}`}>
-            <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar full">Navbar</div>
+            <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar full"><NavBar /></div>
           </div>
 
           <div>
